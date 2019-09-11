36a83f22395ccbdf64f4b2cd4449ae6a3ddcbb20
Lauren Dove <elledoveinc@gmail.com>
2019-09-11 04:11:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  phrase = "Squawk!"
-  puts phrase
-  return phrase
-end
+#def parrot
+  #phrase = "Squawk!"
+ # puts phrase
+  #return phrase
+#end
+
+def parrot("Squawk!")
